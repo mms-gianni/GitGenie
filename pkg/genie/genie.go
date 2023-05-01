b8c41d86bd99f9ea6525e1c135ba831596097efa
Gianni Carafa <gianni.carafa@srf.ch>
2023-05-01 10:47:02 +0200

    initila modules import

---
+++ pkg/genie/genie.go
@@ -0,0 +1,30 @@
+package genie
+
+import (
+	"fmt"
+
+	"github.com/go-git/go-git/v5"
+)
+
+func status() {
+
+	r, err := git.PlainOpen(".")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w, err := r.Worktree()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	status, err := w.Status()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(status)
+}
